c4ff5de0a124b26f80fcf4fac532f8d4a0f5c37f
Mridul Gulati <90506788+Mridul-Gulati@users.noreply.github.com>
2023-08-02 22:15:05 +0530

    Time: 54 ms (97.84%), Space: 14.4 MB (88.00%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a = set(nums)
-        b = []
-        for i in a:
-            b.append(i)
-        b.sort()
-        for i in range(len(b)):
-            nums[i] = b[i]
-        return len(a)
+        nums[:] = sorted(set(nums))
+        
+        return len(nums)

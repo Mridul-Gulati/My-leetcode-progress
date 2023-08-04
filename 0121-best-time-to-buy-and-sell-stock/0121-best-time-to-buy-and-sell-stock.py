b57e81c7f6149db73eb9e5e86bea62b8582341fb
Mridul Gulati <90506788+Mridul-Gulati@users.noreply.github.com>
2023-08-04 19:29:43 +0530

    Time: 805 ms (90.75%), Space: 22.4 MB (79.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        buy_point = 0
-        sell_point = 1
-        profit = 0
-        
-        while buy_point < len(prices) and sell_point < len(prices):
-            if prices[buy_point] > prices[sell_point]:
-                buy_point = sell_point
-                sell_point+=1
+        i = 0
+        j = 1
+        n = len(prices)
+        max_profit = 0
+        while i<n and j<n:
+            profit = prices[j] - prices[i]
+            if profit<=0:
+                i = j
+                j +=1
             else:
-                profit = max(profit,prices[sell_point] - prices[buy_point])
-                sell_point+=1
-        return profit
+                max_profit = max(profit,max_profit)
+                j+=1
+        return max_profit
                 
